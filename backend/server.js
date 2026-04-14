2239b555e964df8423f0dd2844d1518e34441fe3
YashSV-cloud <yash03.inbox@gmail.com>
2026-04-14 13:29:31 +0530

    intial commit for main branch

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,6 @@ app.get("/api/message", (req, res) => {
   res.json({ message: "Hello from Backend 🚀" });
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
